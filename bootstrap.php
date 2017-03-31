319904429be60cb67cc5bde56ca602f0138670ac
Matthew Baggett <matthew@baggett.me>
2017-03-31 13:20:31 +0100

    Add support for running in CLI mode..

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,15 @@
 <?php
 define("TURBO_ROOT", __DIR__);
-define("PUBLIC_ROOT", dirname($_SERVER['SCRIPT_FILENAME']));
+if(php_sapi_name() == 'cli'){
+    define("PUBLIC_ROOT", __DIR__ . "/../../../public");
+}else {
+    define("PUBLIC_ROOT", dirname($_SERVER['SCRIPT_FILENAME']));
+}
 define("APP_ROOT", realpath(PUBLIC_ROOT . "/../"));
+define("APP_CORE_NAME", "TurboCMS\\TurboCMS");
 require_once(APP_ROOT . "/vendor/autoload.php");
 
-\TurboCMS\TurboCMS::Instance()
+$micrositesApp = \TurboCMS\TurboCMS::Instance()
     ->loadAllRoutes()
-    ->getApp()
-        ->run();
+    ->getApp();
+
