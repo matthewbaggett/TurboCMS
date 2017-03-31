84c186f13ff054a6ac78f11752547803cbc30129
Matthew Baggett <matthew@baggett.me>
2017-04-01 00:32:21 +0100

    Initial crappy login stuff. Doesn't actually set a session yet. Looks pretty tho'

--- src/TurboCMS.php
+++ src/TurboCMS.php
@@ -75,9 +75,11 @@ class TurboCMS extends App
         define("APP_NAME", $this->micrositeSelected);
         define("SITE_ROOT", APP_ROOT . "/sites/" . $this->micrositeSelected);
         $this->addViewPath(SITE_ROOT . "/Views");
-        foreach(new \DirectoryIterator(SITE_ROOT . "/Routes") as $file) {
-            if(!$file->isDot() && $file->getExtension() == 'php') {
-                $this->addRoutePath($file->getRealPath());
+        if(file_exists(SITE_ROOT . "/Routes")) {
+            foreach (new \DirectoryIterator(SITE_ROOT . "/Routes") as $file) {
+                if (!$file->isDot() && $file->getExtension() == 'php') {
+                    $this->addRoutePath($file->getRealPath());
+                }
             }
         }
     }
