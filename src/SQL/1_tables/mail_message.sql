707b9789f19f67181613087179f8719bf2f795c9
Matthew Baggett <matthew@baggett.me>
2017-04-25 20:40:42 +0100

    Change subject & message types to BLOB & LONGBLOB

--- src/SQL/1_tables/mail_message.sql
+++ src/SQL/1_tables/mail_message.sql
@@ -5,8 +5,8 @@ CREATE TABLE mailMessage
   hash VARCHAR(64) NOT NULL,
   mailAccountId INT(11) NOT NULL,
   contactId INT(11) NOT NULL,
-  subject TEXT NOT NULL,
-  message BLOB NOT NULL,
+  subject BLOB NOT NULL,
+  message LONGBLOB NOT NULL,
   dateReceived DATETIME NOT NULL,
   dateRead DATETIME,
   `read` ENUM('Unread', 'Read') DEFAULT 'Unread' NOT NULL
