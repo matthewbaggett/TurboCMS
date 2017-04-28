832633561df2ffe19a5ad86ea79e98f6496a3d12
Matthew Baggett <matthew@baggett.me>
2017-04-28 18:29:48 +0100

    Adding Test user.

--- src/SQL/99_data/1_users.sql
+++ src/SQL/99_data/1_users.sql
@@ -1,5 +1,9 @@
+REPLACE INTO `users` (`id`, `uuid`, `email`, `firstName`, `lastName`, `password`, `deleted`) VALUES
+  (NULL, UUID(), 'test@microsites.dev', 'Test', 'User', '', 'Yes');
+
 REPLACE INTO `users` (`id`, `uuid`, `email`, `firstName`, `lastName`, `password`) VALUES
   (NULL, UUID(), 'matthew@baggett.me', 'Matthew', 'Baggett', '$2y$10$1iTYvkHrvtDGvjJgyaGwfuxyzvmgKjYIPzBXV7bbOWbbM/i7m/z1O');
 
 REPLACE INTO `usersSites` (`userId`, `siteId`) VALUES
+  ((SELECT id FROM users where email='test@microsites.dev'),-1),
   ((SELECT id FROM users where email='matthew@baggett.me'),-1);
