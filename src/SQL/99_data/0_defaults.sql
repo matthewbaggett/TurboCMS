ffc0b85e91e551710e0189ff5aafec11aaa3b00c
Matthew Baggett <matthew@baggett.me>
2017-04-28 18:57:42 +0100

    Adding Gallery & Slideshow types

--- src/SQL/99_data/0_defaults.sql
+++ src/SQL/99_data/0_defaults.sql
@@ -1,10 +1,12 @@
 -- Default Page Types
-REPLACE INTO pageTypes VALUES
-  (NULL, 'standard', 'A standard page.'),
-  (NULL, 'blog-post', 'A common or garden variety of blog post.');
+REPLACE INTO pageTypes (name, description) VALUES
+  ('standard', 'A standard page.'),
+  ('blog-post', 'A common or garden variety of blog post.'),
+  ('gallery','An image gallery.'),
+  ('slide-show','A slideshow to show off some images in sequential form.');
 
 -- Default Block Types
-REPLACE INTO blockTypes VALUES
-  (NULL, 'Text', 'A block with paragraphs of text, with an optional Title.'),
-  (NULL, 'Image', 'An Image block, with Alt Text.'),
-  (NULL, 'Video', 'A YouTube video.');
+REPLACE INTO blockTypes (name, description) VALUES
+  ( 'Text', 'A block with paragraphs of text, with an optional Title.'),
+  ( 'Image', 'An Image block, with Alt Text.'),
+  ( 'Video', 'A YouTube video.');
