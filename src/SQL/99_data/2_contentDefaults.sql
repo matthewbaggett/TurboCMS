5873e2586d499f488d2859bde20f23df3975f6fe
Matthew Baggett <matthew@baggett.me>
2017-04-29 02:13:38 +0100

    Add s'more default content, move users.sql

---
+++ src/SQL/99_data/2_contentDefaults.sql
@@ -0,0 +1,10 @@
+INSERT INTO pageTypeSites (pageTypeId, siteId) VALUES
+  ((SELECT id FROM pageTypes WHERE `name`='standard'), (SELECT id FROM sites WHERE siteName = 'default')),
+  ((SELECT id FROM pageTypes WHERE `name`='blog-post'), (SELECT id FROM sites WHERE siteName = 'default')),
+  ((SELECT id FROM pageTypes WHERE `name`='gallery'), (SELECT id FROM sites WHERE siteName = 'default')),
+  ((SELECT id FROM pageTypes WHERE `name`='slide-show'), (SELECT id FROM sites WHERE siteName = 'default'));
+
+INSERT INTO blockTypeSites (blockTypeId, siteId) VALUES
+  ((SELECT id FROM blockTypes WHERE `name` = 'Text'), (SELECT id FROM sites WHERE siteName = 'default')),
+  ((SELECT id FROM blockTypes WHERE `name` = 'Image'), (SELECT id FROM sites WHERE siteName = 'default')),
+  ((SELECT id FROM blockTypes WHERE `name` = 'Video'), (SELECT id FROM sites WHERE siteName = 'default'));
