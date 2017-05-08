4bb243593f168cd04fd952e2da75e89e023914ec
Matthew Baggett <matthew@baggett.me>
2017-05-08 22:22:34 +0100

    Make Mail Fetch and Data Revert into Automizer commands

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,9 @@ if (php_sapi_name() == 'cli') {
     define("PUBLIC_ROOT", dirname($_SERVER['SCRIPT_FILENAME']));
 }
 define("APP_ROOT", realpath(PUBLIC_ROOT . "/../"));
-define("APP_CORE_NAME", "TurboCMS\\TurboCMS");
+if (!defined("APP_CORE_NAME")) {
+    define("APP_CORE_NAME", "TurboCMS\\TurboCMS");
+}
 require_once(APP_ROOT . "/vendor/autoload.php");
 
 $micrositesApp = \TurboCMS\TurboCMS::Instance()
