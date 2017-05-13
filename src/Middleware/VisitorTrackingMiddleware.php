564cb3104e32196e269b0c6e147a6c909d4cb742
Matthew Baggett <matthew@baggett.me>
2017-05-13 15:14:24 +0100

    More tracking data.

--- src/Middleware/VisitorTrackingMiddleware.php
+++ src/Middleware/VisitorTrackingMiddleware.php
@@ -2,34 +2,62 @@
 
 namespace TurboCMS\Middleware;
 
+use Dflydev\FigCookies;
+
 use Segura\AppCore\App;
 use Slim\Http;
 use Thru\UUID\UUID;
 
 class VisitorTrackingMiddleware{
+
+
+    private $ignoredPaths = [
+        "/asset",
+        "/image",
+        "/demo",
+    ];
+
     public function __invoke(Http\Request $request, Http\Response $response, $next)
     {
-
-        /** @var Http\Response $response */
-        $response = $next($request, $response);
+        foreach($this->ignoredPaths as $ignoredPath){
+            if(stripos($request->getUri()->getPath(), $ignoredPath) === 0){
+                return $next($request, $response);
+            }
+        }
+        $cookieName = crc32("Tracking");
+        if(FigCookies\FigRequestCookies::get($request, $cookieName)->getValue() == null) {
+            $trackingId = UUID::v4();
+            $setCookie = FigCookies\SetCookie::create($cookieName)
+                ->withValue($trackingId)
+                ->rememberForever()
+                ->withPath('/');
+            $response = FigCookies\FigResponseCookies::set($response, $setCookie);
+        }
         /** @var \Predis\Client $redis */
         $redis = App::Container()->get("Redis");
-        /** @var Http\Cookies $cookies */
-        $cookies = App::Container()->get("Cookies");
 
-        if(!$cookies->get("Tracking")) {
-            $cookies->set("Tracking", UUID::v4());
-        }
+        $visitorId = FigCookies\FigRequestCookies::get($request, $cookieName)->getValue();
+        $requestPath = $request->getUri()->__toString();
 
-        \Kint::dump(
-            $request->getCookieParams(),
-            $request->getHeaders(),
-            $cookies
+        $redis->incr("track:{$visitorId}:page_views");
+        $redis->hset(
+            "track:{$visitorId}:pages",
+            $redis->get("track:{$visitorId}:page_views"),
+            json_encode(
+                [
+                    'path' => $requestPath,
+                    'method' => $request->getMethod(),
+                    'time' => date("Y-m-d H:i:s")
+                ],
+                JSON_PRETTY_PRINT
+            )
         );
 
-        $visitorId = $cookies->get("Tracking");
-        $redis->incr("track:{$visitorId}:page_views");
+        $response = $next($request, $response);
 
+        if(isset($setCookie)){
+            $response = FigCookies\FigResponseCookies::set($response, $setCookie);
+        }
 
         return $response;
     }
