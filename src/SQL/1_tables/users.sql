734f9fa9ec22f69bf7798a0f9e73612f7ec7d2dd
Matthew Baggett <matthew@baggett.me>
2017-05-14 10:54:43 +0100

    Change order to make phpmyadmin happier.

--- src/SQL/1_tables/users.sql
+++ src/SQL/1_tables/users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uuid` VARCHAR(36) NOT NULL,
   `email` varchar(320) NOT NULL,
+  `uuid` VARCHAR(36) NOT NULL,
   `firstName` VARCHAR(255) NULL,
   `lastName` VARCHAR(255) NOT NULL,
   `password` text NOT NULL,
