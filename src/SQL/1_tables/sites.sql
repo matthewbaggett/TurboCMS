89c0e6535965e3a0c552a579bffd1941b296baff
Matthew Baggett <matthew@baggett.me>
2017-05-22 00:15:08 +0100

    Implement bandwidth and storage limits against site record

--- src/SQL/1_tables/sites.sql
+++ src/SQL/1_tables/sites.sql
@@ -4,6 +4,9 @@ CREATE TABLE `sites` (
   `siteTitle` varchar(255) NOT NULL,
   `uuid` VARCHAR(36) NOT NULL,
   `views` INT(11) NOT NULL DEFAULT 0,
+  `maxPages` INT(11) NOT NULL DEFAULT 100,
+  `maxDiskUsageMegaBytes` INT(11) NOT NULL DEFAULT 100,
+  `maxBandwidthUsageMegaBytes` INT(11) NOT NULL DEFAULT 100,
   `deleted` ENUM('Yes','No') DEFAULT 'No'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
