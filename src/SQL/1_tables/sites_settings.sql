9335cd415ec820ef4cba7e2ddb8940fb5414b297
Matthew Baggett <matthew@baggett.me>
2017-05-29 23:56:00 +0100

    Change value to be a text.

--- src/SQL/1_tables/sites_settings.sql
+++ src/SQL/1_tables/sites_settings.sql
@@ -3,7 +3,7 @@ CREATE TABLE sitesSettings
   `id` INT PRIMARY KEY AUTO_INCREMENT,
   `siteId` INT,
   `key` VARCHAR(255),
-  `value` VARCHAR(255),
+  `value` TEXT,
   KEY `siteId` (`siteId`),
   KEY `key` (`key`)
 );
